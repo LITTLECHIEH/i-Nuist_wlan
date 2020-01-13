ba7d3fe87f13824ea7c764c350af62be418a197a
LITTLECHIEH <45623023+LITTLECHIEH@users.noreply.github.com>
2020-01-13 11:58:17 +0800

    fix WiFi.py
    add notes. format codes

--- WiFi.py
+++ WiFi.py
@@ -13,6 +13,11 @@ domain = ""
 
 
 def init_account():
+    """
+    初始化账户，启动软件之后，检查是否已经存在用户信息。如果已经存在，则初始化用户信息。
+
+    :return: 如果存在，则返回True。
+    """
     dir_list = os.listdir()
     if "config" not in dir_list:  # 查看文件夹是否存在
         os.mkdir("config")
@@ -29,36 +34,18 @@ def init_account():
 
 
 def save_account():
+    """
+    储存用户信息
+    """
     acccount = {'username': username, 'password': password, 'domain': domain}
     output = open("config/account.json", 'w+')
     print(acccount, file=output)
 
 
-def create_shortcut_to_startup():
-    target = path.curdir+"\\WiFi.exe"
-    print(target)
-    title = '我的快捷方式'
-
-    winshell.CreateShortcut(
-        Path=path.join(winshell.startup(), "NUIST WiFi" + '.lnk'),
-        Target=target,
-        Icon=(target, 0),
-        Description=title)
-
-
-def create_shortcut_to_desktop():
-    target = path.curdir+"\\WiFi.exe"
-    print(target)
-    title = '我的快捷方式'
-
-    winshell.CreateShortcut(
-        Path=path.join(winshell.desktop(), "NUIST WiFi" + '.lnk'),
-        Target=target,
-        Icon=(target, 0),
-        Description=title)
-
-
 def input_infor():
+    """
+    输入用户信息以及配置项
+    """
     global username, password, domain
     username = input("请输入账号：")
     password = getpass.getpass("请输入密码（不显示）：")
@@ -72,13 +59,40 @@ def input_infor():
         create_shortcut_to_desktop()
 
 
-def run():
+def create_shortcut_to_startup():
+    """
+    设置开机自启
+    """
+    target = path.curdir + "\\WiFi.exe"
+    print(target)
+    title = '我的快捷方式'
+
+    winshell.CreateShortcut(
+        Path=path.join(winshell.startup(), "NUIST WiFi" + '.lnk'),
+        Target=target,
+        Icon=(target, 0),
+        Description=title)
 
 
-    if init_account():  # 若没有输入过密码，则会请求输入
-        pass
-    else:
-        input_infor()
+def create_shortcut_to_desktop():
+    """
+    创建桌面快捷方式
+    """
+    target = path.curdir + "\\WiFi.exe"
+    print(target)
+    title = '我的快捷方式'
+
+    winshell.CreateShortcut(
+        Path=path.join(winshell.desktop(), "NUIST WiFi" + '.lnk'),
+        Target=target,
+        Icon=(target, 0),
+        Description=title)
+
+
+def connect_login():
+    """
+    登录以及连接 wifi
+    """
     print("正在等待设备准备ing")
     time.sleep(2)
     connect_wifi('i-NUIST')  # 连接Wifi
@@ -86,7 +100,7 @@ def run():
     certification = Certification(username=username, password=password, domain=domain)
 
     if certification.wlan_login():
-        os.system('cls')    # 清空命令行
+        os.system('cls')  # 清空命令行
         print("成功登录！！")
         print("成功登录！！")
         time.sleep(0.5)
@@ -95,8 +109,15 @@ def run():
         print("登录失败")
         time.sleep(1)
         os.system('cls')
-        run()   # 重新输入
+        run()  # 重新输入
 
+
+def run():
+    if init_account():  # 若没有输入过密码，则会请求输入
+        pass
+    else:
+        input_infor()  # 初始化用户信息以及配置（要求用户输入）
+    connect_login()     # 连接wifi以及登录校园网
 
 
 if __name__ == '__main__':
